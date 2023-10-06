8bd9c09093a55977d67a74b6ff5e96540d77f7c5
brian054 <brian.rowell54@gmail.com>
2023-10-05 22:08:02 -0500

    Houston, we have gravity

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-window.onload = function() {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-    const backgroundImage = new Image();
-    backgroundImage.src = 'images/background.jpeg';
+/* 
+Friday TODO: 
+    - Add a ReadMe file that documents how to run project
+    - Collision with ground
+    - Background scrolling on inf loop
+    - Create Pipes
+    - Set collision on pipes
+    - Infinite Pipes
+    - Score
+*/
 
-    backgroundImage.onload = function() {
-        ctx.drawImage(backgroundImage, 0, 0, backgroundImage.width, backgroundImage.height);
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-        // Draw Flappy
-        ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.fillRect(canvas.width / 2, canvas.height / 2, 40, 40);
-        ctx.stroke();
-    }
+const backgroundImage = new Image();
+backgroundImage.src = 'images/background.jpeg';
+
+const rectWidth = 40;
+const rectHeight = 40;
+let rectX = canvas.width / 2 - rectWidth / 2;
+let rectY = canvas.height / 2 - rectHeight / 2;
+const gravity = 6;
+
+backgroundImage.onload = function() {
+    update(); 
+}
+
+function update() {
+    ctx.drawImage(backgroundImage, 0, 0, backgroundImage.width, backgroundImage.height);   
+
+    // Apply gravity to the rectangle
+    rectY += gravity;
+
+     // Draw Flappy
+     ctx.beginPath();
+     ctx.fillStyle = "red";
+     ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
+     ctx.stroke();
+
+     requestAnimationFrame(update);
 }
